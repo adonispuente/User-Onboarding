b567d1af2c40ac8a6e9ccec301e5fe44439d5520
Adonis Puente <adonispuente@Adoniss-Air.attlocal.net>
2020-03-31 17:45:09 -0400

    done, no stretch

---
+++ user-onboarding/cypress/integration/form_test.js
@@ -0,0 +1,19 @@
+describe("Testing our volunteer form", function() {
+  beforeEach(function() {
+    cy.visit("http://localhost:3000");
+  });
+  it("Add test to inputs and submit form", function() {
+    cy.get('input[name="name"]').type("Adonis");
+    cy.get("input[name='email']")
+      .type("email@email.com")
+      .should("have.value", "email@email.com");
+    cy.get('input[name="password"]')
+      .type("blabla")
+      .should("have.value", "blabla");
+
+    cy.get("[type=checkbox]")
+      .check()
+      .should("be.checked");
+    cy.get("button").click();
+  });
+});
